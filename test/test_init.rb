e40939946a14d0d081776a157f079a7829626a88
Nathan Ladd <nathanladd@gmail.com>
2023-07-25 08:54:12 -0500

    test/test_init.rb is updated to use TestBenchIsolated

--- test/test_init.rb
+++ test/test_init.rb
@@ -1,12 +1,6 @@
-ENV['BOOTSTRAP'] ||= 'on'
-
 require_relative '../init'
 
-if ENV['BOOTSTRAP'] == 'on'
-  require 'test_bench/bootstrap'; TestBench::Bootstrap.activate
-else
-  require 'test_bench'; TestBench.activate
-end
+require 'test_bench/isolated'; TestBenchIsolated::TestBench.activate
 
 require 'TEMPLATE-PATH/controls'
 
