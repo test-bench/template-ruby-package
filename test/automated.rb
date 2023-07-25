2152584af0c57cee31658a498ee873ac225e1e84
Nathan Ladd <nathanladd@gmail.com>
2023-07-25 10:11:44 -0500

    test/automated.rb is updated to use TestBenchIsolated

--- test/automated.rb
+++ test/automated.rb
@@ -1,11 +1,6 @@
 require_relative 'test_init'
 
-if ENV['BOOTSTRAP'] == 'on'
-  TestBench::Bootstrap::Run.()
-  return
-end
-
-TestBench::Run.(
+TestBenchIsolated::TestBench::Run.(
   'test/automated',
   exclude_file_pattern: '**/{_*,sketch*,*_init.rb,*_tests.rb}'
 ) or exit(false)
