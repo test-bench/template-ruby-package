d4c1abbc1a0e38986d4decad70026393ab475e05
Nathan Ladd <nathanladd@gmail.com>
2023-07-26 14:18:17 -0500

    Exclude file pattern is corrected

--- test/automated.rb
+++ test/automated.rb
@@ -2,5 +2,5 @@ require_relative 'test_init'
 
 TestBenchIsolated::TestBench::Run.(
   'test/automated',
-  exclude_file_pattern: '**/{_*,sketch*,*_init.rb,*_tests.rb}'
+  exclude: '{_*,*sketch*,*_init,*_tests}.rb'
 ) or exit(false)
